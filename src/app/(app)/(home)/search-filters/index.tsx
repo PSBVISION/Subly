82ddaa95be45b3a6aad230866d19d93ae1e34997
PSBVISION <punitsinghbisht1529@gmail.com>
2026-04-18 23:30:08 +0530

    feat: update CategoryDropdown and SubcategoryMenu for improved navigation and linking

--- src/app/(app)/(home)/search-filters/index.tsx
+++ src/app/(app)/(home)/search-filters/index.tsx
@@ -10,7 +10,9 @@ export const SearchFilters = ({ data }: SearchFiltersProps) => {
   return (
     <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
       <SearchInput />
+      <div className="hidden lg:block">
       <Categories data={data} />
+      </div>
     </div>
   );
 };
