01fd47d029716994183df22a154d2a398d4d6539
PSBVISION <punitsinghbisht1529@gmail.com>
2026-04-29 23:50:27 +0530

    feat: add TRPC dependencies and enhance SearchInput component layout

--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -18,12 +18,25 @@ export const SearchInput = ({ disabled, data }: SearchInputProps) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
     <div className="flex items-center gap-2 w-full">
-      <CategoriesSidebar data={data} open={isSidebarOpen} onOpenChange={setIsSidebarOpen}/>
+      <CategoriesSidebar
+        data={data}
+        open={isSidebarOpen}
+        onOpenChange={setIsSidebarOpen}
+      />
       <div className="relative w-full">
-        <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" /> <Input className="px-8" placeholder="Search Products" disabled={disabled} />
+        <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />{" "}
+        <Input
+          className="px-8"
+          placeholder="Search Products"
+          disabled={disabled}
+        />
       </div>
-      <Button variant="elevated" className="shrink-0 size-12 flex lg:hidden" onClick={()=> setIsSidebarOpen(true)}>
-<ListFilterIcon/>
+      <Button
+        variant="elevated"
+        className="shrink-0 size-12 flex lg:hidden"
+        onClick={() => setIsSidebarOpen(true)}
+      >
+        <ListFilterIcon />
       </Button>
     </div>
   );
